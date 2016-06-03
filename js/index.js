e684a311d428a8b951235f42972dcba599ee22a9
f245 <f245@ivkhk.local>
2016-06-03 11:16:06 +0300

    валидация формы логин и пароль на пустое поле

---
+++ js/index.js
@@ -0,0 +1,5 @@
+alert( 'Привет, Мир!' );
+document.getElementById('nav').innerHTML="This text was made by JS";
+document.getElementById('my-button').ondblclick = function(event) {
+	alert("Ты нажал на кнопку");
+}
