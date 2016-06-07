9df328ab9c37c90467e125a189d6201aa0d92698
f245 <f245@ivkhk.local>
2016-06-07 14:55:47 +0300

    second lessson

--- js/index.js
+++ js/index.js
@@ -1,5 +1,63 @@
-alert( 'Привет, Мир!' );
 document.getElementById('nav').innerHTML="This text was made by JS";
 document.getElementById('my-button').ondblclick = function(event) {
 	alert("Ты нажал на кнопку");
+}
+ function showError(container, errorMessage) {
+      container.className = 'error';
+      var msgElem = document.createElement('span');
+      msgElem.className = "error-message";
+      msgElem.innerHTML = errorMessage;
+      container.appendChild(msgElem);
+    }
+
+    function resetError(container) {
+      container.className = '';
+      if (container.lastChild.className == "error-message") {
+        container.removeChild(container.lastChild);
+      }
+    }
+
+    function validate(form) {
+      var elems = form.elements;
+
+      resetError(elems.from.parentNode);
+      if (!elems.from.value) {
+        showError(elems.from.parentNode, ' Укажите логин.');
+      }
+
+      resetError(elems.password.parentNode);
+      if (!elems.password.value) {
+        showError(elems.password.parentNode, ' Укажите пароль.');
+      }
+
+    }
+
+    function validation() {
+    	var login=document.getElementById('_login');
+    	var pass=document.getElementById('_pass');
+    	var flagLogin=false;
+    	var flagPass=false;
+
+    	if (login.value=='') {
+    		flagLogin = false;
+    		login.classList.remove('red-border');
+    	} else {
+    		flagLogin = true;
+    	}
+
+    	if (pass.value=='') {
+        flagPass = false;
+        pass.classList.add('red-border');
+    	} else {
+    		flagPass = true;
+    		pass.classList.remove('red-border');
+    	}
+
+    	if(flagLogin==true && flagPass==true)
+    	{
+    		return true;
+    	} else
+    	{
+    		return false;
+    	}
 }
